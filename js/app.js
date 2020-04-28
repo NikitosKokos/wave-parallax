2eefaa9de267a02024995cb57d21ad480e29506f
Nikita <61088793+NikitosKokos@users.noreply.github.com>
2020-04-28 10:32:06 +0300

    Commit 2

--- js/app.js
+++ js/app.js
@@ -1,26 +1,60 @@
-var myWave = $('#myID').wavify({
-    height: 550,
-    bones: 6,
-    amplitude: 80,
-    color: '#62a6ff',
-    speed: .25
-  });
-  var myWave = $('#svg').wavify({
-    height: 50,
-    bones: 5,
-    amplitude: 100,
-    color: '#111',
-    speed: .25
-  });
+// let ch = document.documentElement.clientHeight;
+// let cw = document.documentElement.clientWidth;
+// console.log(ch, cw);
 
-  $(window).scroll(function(event){
-    let s = $(this).scrollTop();
-    let w = $(this).outerWidth();
-    let h = $(".wrapper").outerHeight();
-    let p = s/h*100;
-    let t = p;
-    // console.log(t);
-    // console.log(s);
-    console.log(p);
-    $(".svg-parallax").css('transform', `translateY(${100-p}%)`)
-  });
+var myWave = $("#myID").wavify({
+  height: 550,
+  bones: 6,
+  amplitude: 80,
+  color: "#62a6ff",
+  speed: 0.25,
+});
+var myWave = $("#svg").wavify({
+  height: 50,
+  bones: 5,
+  amplitude: 100,
+  color: "#111",
+  speed: 0.25,
+});
+
+$(window).scroll(function (event) {
+  let s = $(this).scrollTop();
+  let w = $(this).outerWidth();
+  let h = $(".wrapper").outerHeight();
+  let p = (s / h) * 100;
+  let t = p;
+  if (p > 0) {
+    h = document.querySelector(".header");
+    h.classList.add("header_active");
+  } else {
+    h = document.querySelector(".header");
+    h.classList.remove("header_active");
+  }
+  if (p >= 60) {
+    h = document.querySelector(".header");
+    h.classList.add("header_active-white");
+  } else {
+    h = document.querySelector(".header");
+    h.classList.remove("header_active-white");
+  }
+  // console.log(t);
+  // console.log(s);
+  console.log(p);
+  $(".svg-parallax").css("transform", `translateY(${100 - p}%)`);
+});
+
+setInterval(() => {
+  const deg = 6;
+  const hr = document.querySelector("#hr");
+  const mn = document.querySelector("#mn");
+  const sc = document.querySelector("#sc");
+
+  let day = new Date();
+  let hh = day.getHours() * 30;
+  let mm = day.getMinutes() * deg;
+  let ss = day.getSeconds() * deg;
+
+  hr.style.transform = `rotateZ(${hh + mm / 12}deg)`;
+  mn.style.transform = `rotateZ(${mm}deg)`;
+  sc.style.transform = `rotateZ(${ss}deg)`;
+});
